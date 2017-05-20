460ab8178b59a3e6e2805b095dac9892996ebce5
Carrie Ybay <carrieybay@gmail.com>
2017-05-20 16:17:43 -0700

    Initial commit

---
+++ calc.c
@@ -0,0 +1,56 @@
+#include "monty.h"
+
+/**
+ * add - adds top two elements of stack, removes top element, and replaces
+ * the value of the top element with the sum
+ * Return: 0 on success, -1 on failure
+ */
+int add(void)
+{
+
+	return (0);
+}
+
+/**
+ * sub - subtracts top two elements of stack, removes top element, and replaces
+ * the value of the top element with the difference
+ * Return: 0 on success, -1 on failure
+ */
+int sub(void)
+{
+
+	return (0);
+}
+
+/**
+ * mul - multiplies top two elements of stack, removes top element, and replaces
+ * the value of the top element with the product
+ * Return: 0 on success, -1 on failure
+ */
+int mul(void)
+{
+
+	return (0);
+}
+
+/**
+ * div - divides top two elements of stack, removes top element, and replaces
+ * the value of the top element with the quotient
+ * Return: 0 on success, -1 on failure
+ */
+int div(void)
+{
+
+	return (0);
+}
+
+/**
+ * mod - divides top two elements of stack, removes top element, and replaces
+ * the value of the top element with the remainder
+ * Return: 0 on success, -1 on failure
+ */
+int mod(void)
+{
+
+	return (0);
+}
