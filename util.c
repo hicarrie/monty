c1135a332f88eae2a06e3b5447c9f6b0a4547b54
Carrie Ybay <carrieybay@gmail.com>
2017-05-20 16:22:11 -0700

    Initial commit

---
+++ util.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * pall - prints the value of all items on the stack
+ * Return: 0 on success, -1 on failure
+ */
+int pall(void)
+{
+
+}
+
+/**
+ * pint - returns the value of the top element of the stack
+ * Return: 0 on success, -1 on failure
+ */
+int pint(void)
+{
+
+}
